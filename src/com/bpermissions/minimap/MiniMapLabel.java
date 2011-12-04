0ddcdb3ed89557736b353bba291f74555eaec0ea
mbsuperstar1 <subscribeaholic@live.co.uk>
2011-12-04 00:35:42 +0000

    Basic Coordinates...

--- src/com/bpermissions/minimap/MiniMapLabel.java
+++ src/com/bpermissions/minimap/MiniMapLabel.java
@@ -1,27 +1,31 @@
 package com.bpermissions.minimap;
 
-import org.spoutcraft.spoutcraftapi.gui.Color;
+import org.spoutcraft.spoutcraftapi.addon.java.JavaAddon;
+import org.spoutcraft.spoutcraftapi.entity.ActivePlayer;
 import org.spoutcraft.spoutcraftapi.gui.GenericLabel;
-import org.spoutcraft.spoutcraftapi.util.FixedLocation;
 
 public class MiniMapLabel extends GenericLabel {
+	private final JavaAddon parent;
 	
-	MiniMapAddon parent;
-	FixedLocation loc;
 	public MiniMapLabel(MiniMapAddon parent) {
-		super("0, 0, 0");
-		this.setVisible(true);
 		this.parent = parent;
-		this.setTextColor(new Color(255, 255, 255, 255));
+		//this.setTextColor(new Color(255, 255, 255, 0));
+		//You had alpha at max, also this currently is an odd
+		//black, so I left it white.
 	}
-
+	
+	@Override
+	public void onTick() {
+		ActivePlayer player = parent.getClient().getActivePlayer();
+		this.setText("X: " + Math.round(player.getLocation().getX()) 
+				+ " Y: " + Math.round(player.getLocation().getY()) 
+				+ " Z: " + Math.round(player.getLocation().getZ()));
+	}
+	
 	@Override
 	public void render() {
-		this.setX((int) (this.getScreen().getWidth()/2));
-		this.setY((int) (this.getScreen().getHeight()/2));
-		loc = parent.getClient().getActivePlayer().getLocation();
-		System.out.println(this.getText());
-		this.setText(loc.getBlockX()+", "+loc.getBlockY()+", "+loc.getBlockZ());
+		int x = (int) this.getScreen().getWidth()-100;
+		this.setX(x).setY(MiniMap.width-20);
 		super.render();
 	}
 
