1f8c39a9cf1acdf38f092111e533e9b710962370
xXKeyleXx <keyle@t-online.de>
2012-01-16 11:22:55 +0100

    Added small note ("S") for slimechunks right next to the coordinates.

--- src/com/bpermissions/minimap/MiniMapLabel.java
+++ src/com/bpermissions/minimap/MiniMapLabel.java
@@ -1,5 +1,7 @@
 package com.bpermissions.minimap;
 
+import java.util.Random;
+
 import org.spoutcraft.spoutcraftapi.ChatColor;
 import org.spoutcraft.spoutcraftapi.Spoutcraft;
 import org.spoutcraft.spoutcraftapi.gui.GenericLabel;
@@ -8,7 +10,8 @@ import org.spoutcraft.spoutcraftapi.util.FixedLocation;
 public class MiniMapLabel extends GenericLabel {
 	private final MiniMapAddon parent;
 	private static final String COORDS = ChatColor.YELLOW + "(%d, %d, %d)";
-
+	private static final String COORDS_SLIME = "(%d, %d, %d, S)";
+	
 	public MiniMapLabel(MiniMapAddon parent) {
 		this.parent = parent;
 	}
@@ -16,7 +19,12 @@ public class MiniMapLabel extends GenericLabel {
 	@Override
 	public void onTick() {
 		FixedLocation loc = parent.getClient().getActivePlayer().getLocation();
-		setText(String.format(COORDS, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+
+		if (canSlimeSpawn(loc)) {
+			setText(String.format(COORDS_SLIME, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+		} else {
+			setText(String.format(COORDS, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+		}
 	}
 
 	@Override
@@ -25,5 +33,12 @@ public class MiniMapLabel extends GenericLabel {
 		final int x = (int) Math.round(MiniMapWidget.getStaticWidth() / 2 + MiniMapWidget.getStaticX() - Spoutcraft.getMinecraftFont().getTextWidth(this.getText()) / 2);
 		this.setX(x).setY((int) (MiniMapWidget.getStaticWidth()));
 		super.render();
+	}
+
+	private boolean canSlimeSpawn(FixedLocation loc) {
+		int x = loc.getBlock().getChunk().getX();
+		int z = loc.getBlock().getChunk().getZ();
+		Random rnd = new Random(loc.getWorld().getSeed() + x * x * 4987142 + x * 5947611 + z * z * 4392871L + z * 389711 ^ 987234911L);
+		return rnd.nextInt(10) == 0;
 	}
 }
