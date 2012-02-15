07a31411f8a56e91ab040e774bde116f708e21ec
Fabian Neundorf <CommodoreFabianus@gmx.de>
2012-02-15 13:51:00 +0100

    !correct position of the label with different gui scales;

--- src/com/bpermissions/minimap/MiniMapLabel.java
+++ src/com/bpermissions/minimap/MiniMapLabel.java
@@ -7,9 +7,8 @@ import org.spoutcraft.spoutcraftapi.util.FixedLocation;
 
 public class MiniMapLabel extends GenericLabel {
 	private final MiniMapAddon parent;
-	public static boolean show = true;
-	private final String coords = ChatColor.YELLOW + "(%d, %d, %d)";
-	
+	private static final String COORDS = ChatColor.YELLOW + "(%d, %d, %d)";
+
 	public MiniMapLabel(MiniMapAddon parent) {
 		this.parent = parent;
 	}
@@ -17,26 +16,14 @@ public class MiniMapLabel extends GenericLabel {
 	@Override
 	public void onTick() {
 		FixedLocation loc = parent.getClient().getActivePlayer().getLocation();
-		setText(String.format(coords, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
-	}
-
-	
-	public boolean isVisible() {
-		return super.isVisible() && show;
-	}
-	
-	public double getWidthX() {
-		return Spoutcraft.getRenderDelegate().getScreenWidth();
-	}
-
-	public double getHeightY() {
-		return Spoutcraft.getRenderDelegate().getScreenHeight();
+		setText(String.format(COORDS, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
 	}
 
 	@Override
 	public void render() {
-		int x = (int) getWidthX()-13-Spoutcraft.getMinecraftFont().getTextWidth(this.getText());
-		this.setX(x).setY((int) (getWidthX()/5));
+//		int x = (int) getWidthX()-13-Spoutcraft.getMinecraftFont().getTextWidth(this.getText());
+		final int x = (int) Math.round(MiniMapWidget.getStaticWidth() / 2 + MiniMapWidget.getStaticX() - Spoutcraft.getMinecraftFont().getTextWidth(this.getText()) / 2);
+		this.setX(x).setY((int) (MiniMapWidget.getStaticWidth()));
 		super.render();
 	}
 }
